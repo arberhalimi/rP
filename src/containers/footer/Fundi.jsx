c5bf496826bb9f31469a162a6928249885885221
Arbër Halimi <arberhalimi1@outlook.com>
2023-02-13 17:22:22 +0100

    components

---
+++ src/containers/footer/Fundi.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    <footer className='container footer'>
+        <div className='logo'>rP</div>
+        <div>All rights reserved &copy;</div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./possibility.css"
+import pImage from "../../assets/possibility.png"
+
+const Possibility = () => {
+  return (
+    <section className="container possibility">
+      <div className="possibility-image">
+        <img src={pImage} alt="" />
+      </div>
+      <div className="possibility-text">
+        <a href="#">Request Early Access to Get Started</a>
+        <h1>The possibilities are beyond your imagination</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      </div>
+    </section>
+  )
+}
+
+export default Possibility
